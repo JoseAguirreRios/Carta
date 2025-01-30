6d678da3b461d78438dcd0661f4fcb39a16c7ec8
Jose Andres Aguirre Rios <josereach.14@gmail.com>
2025-01-29 21:20:46 -0600

    commit first

--- src/Carta.java
+++ src/Carta.java
@@ -4,4 +4,18 @@ import java.util.List;
 import java.util.Random;
 
 public class Carta {
+    private String palo;
+    private String color;
+    private String valor;
+
+    public Carta(String palo, String color, String valor){
+        this.palo = palo;
+        this.color = color;
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString(){
+        return palo + "," + ","  + valor;
+    }
 }
